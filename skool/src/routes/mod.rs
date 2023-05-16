25ef30547b304ec90bbb1d4454dd3bd28f73ccd6
akeamc <ake@amcoff.net>
2023-05-16 21:31:34 +0200

    remove dead code

--- skool/src/routes/mod.rs
+++ skool/src/routes/mod.rs
@@ -26,22 +26,6 @@ pub async fn get_health() -> impl IntoResponse {
     ([("cache-control", "no-cache")], Json(Health::default()))
 }
 
-// fn config_traced(cfg: &mut web::ServiceConfig) {
-//     cfg.service(web::scope("/schedule").configure(schedule::config))
-//         .service(web::scope("/credentials").configure(credentials::config))
-//         .service(web::scope("/classes").configure(classes::config));
-// }
-
-// pub fn config(cfg: &mut web::ServiceConfig) {
-//     cfg.service(web::resource("/health").route(web::get().to(get_health)))
-//         .service(
-//             // don't trace health checks
-//             web::scope("")
-//                 .wrap(RequestTracing::new())
-//                 .configure(config_traced),
-//         );
-// }
-
 pub fn app() -> Router<AppState> {
     Router::<_>::new()
         .nest("/schedule", schedule::routes())
